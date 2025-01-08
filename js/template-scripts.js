6abc28ff0937afbf2bfe62d1ac191be72136625d
empireones <empireones@gmail.com>
2025-01-08 14:44:52 -0500

    Add files via upload

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -24,3 +24,14 @@ jQuery(document).ready(function($) {
         document.querySelector('footer.grid').innerHTML = html;
       });
 
+const navToggle = document.querySelector('.my-nav-toggle');
+const navLinks = document.querySelector('.my-nav-links');
+
+navToggle.addEventListener('click', () => {
+  // Toggle the 'aria-expanded' attribute
+  const expanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
+  navToggle.setAttribute('aria-expanded', !expanded);
+
+  // Toggle the visibility of the navigation links
+  navLinks.hidden = !navLinks.hidden;
+});
